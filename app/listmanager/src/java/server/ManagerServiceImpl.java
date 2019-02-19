c23c0d268d04748f7ef68b6219b2ef58f994d48e
Dyatlov Mikhail <neutralmike@yandex.ru>
2019-02-19 11:35:32 +0300

    listmanager now

--- app/listmanager/src/java/server/ManagerServiceImpl.java
+++ app/listmanager/src/java/server/ManagerServiceImpl.java
@@ -1,7 +1,7 @@
-package server;
+package java.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-import client.ManagerService;
+import java.client.ManagerService;
 
 public class ManagerServiceImpl extends RemoteServiceServlet implements ManagerService {
     public String getMessage(String msg) {
