6c1c39ab0eeb905361cb431d072d73914851046f
Dyatlov Mikhail <neutralmike@yandex.ru>
2019-02-21 13:14:10 +0300

    Check that the type name 'ru.dyatlov.model.client.DB' is really what you meant

---
+++ modules/model/src/main/java/ru.dyatlov.model/client/DB.java
@@ -0,0 +1,10 @@
+package ru.dyatlov.model.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+public class DB implements EntryPoint {
+    public void onModuleLoad() {
+
+    }
+}
