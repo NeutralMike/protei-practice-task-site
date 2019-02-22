d2b30952b49bba10d83ef2f9d19b99d068358033
Dyatlov Mikhail <neutralmike@yandex.ru>
2019-02-22 15:01:35 +0300

    still dont work

--- app/listmanager/src/main/java/ru/dyatlov/listmanager/client/Manager.java
+++ app/listmanager/src/main/java/ru/dyatlov/listmanager/client/Manager.java
@@ -5,30 +5,11 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import ru.dyatlov.model.client.TakeTable;
-import ru.dyatlov.listmanager.client.ui.Table;
+import ru.dyatlov.listmanager.client.ui.*;
 
 public class Manager implements EntryPoint {
 
     public void onModuleLoad() {
-
-            Table table = new Table();
-            RootPanel.get("deleteButton").add(table);
-
+        RootPanel.get().add(new Table());
     }
-
-//    private static class ManagerAsyncCallback implements AsyncCallback<String> {
-//        private Label label;
-//
-//        ManagerAsyncCallback(Label label) {
-//            this.label = label;
-//        }
-//
-//        public void onSuccess(String result) {
-//            label.getElement().setInnerHTML(result);
-//        }
-//
-//        public void onFailure(Throwable throwable) {
-//            label.setText("Failed to receive answer from ru.dyatlov.listmanager.server!");
-//        }
-//    }
 }
